f3a3d8c7e594ad12cedef6dc30afac344f9d693e
Lyndsay Fearnall <lyndsayfearnall@Lyndsays-MacBook-Pro-2.local>
2017-11-30 21:12:15 -0500

    JS with CSS

--- js/main.js
+++ js/main.js
@@ -1,23 +1,35 @@
 (function () {
   // start with retrieving the elements from the page, and then adding event handling. then write the logic. refer to the seasons example / homework
-  var carImages = document.querySelectorAll(".data-ref"),
+  const carImages = document.querySelectorAll(".data-ref"),
       modelName = document.querySelector(".modelName"),
       modelPrice = document.querySelector(".priceInfo"),
       modelDesc = document.querySelector(".modelDetails");
 
 
-function changeText(){
+function changeText(e){
   objectIndex = carData[this.id];
   //console.log("from carImages");
   modelName.firstChild.nodeValue = objectIndex.name;
   modelDesc.firstChild.nodeValue = objectIndex.details;
   modelPrice.firstChild.nodeValue = objectIndex.price;
+
+  //select all elements with active class
+    const active = document.querySelectorAll(".focusMini");
+
+      active.forEach(function(el){
+      //for elements that are active, remove focusMini and add nonActive
+        el.classList.remove('focusMini');
+        el.classList.add('nonActive');
+      })
+
+      //remove nonActive and add focusMini class
+      e.target.classList.remove('nonActive');
+      e.target.classList.add('focusMini');
 }
 
 for (var i = 0; i < carImages.length; i++){
   carImages[i].addEventListener('click', changeText);
 }
-
 
 })();
 
